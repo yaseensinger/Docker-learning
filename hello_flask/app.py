104f35c2d139d45b9c1c9993f3209dd6e47e6d3a
yaseensinger <yaseensinger@hotmail.com>
2026-07-07 21:34:06 +0100

    hello flask

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def hello_world():
     # Connect to the MySQL database
     db = MySQLdb.connect(
-        host="mydb",    # Hostname of the MySQL container
+        host="ddb",    # Hostname of the MySQL container
         user="root",    # Username to connect to MySQL
         passwd="my-secret-pw",  # Password for the MySQL user
         db="mysql"      # Name of the database to connect to
